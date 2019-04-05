256d458eed02cc33aab22845d6d7e470af29c0c0
brunoluizs <y.bruno@live.com>
2019-04-05 19:13:25 -0300

    1.1.1: imagem expandida

--- opencv/main.cpp
+++ opencv/main.cpp
@@ -1,19 +1,46 @@
 #include <opencv.hpp>
+#include <math.h>
 
 using namespace cv;
+using namespace std;
+
+Mat expandImgSize(Mat img){
+	Mat expanded_img;
+
+	//Funções para o cálculo do tamanho ideal para a DFT
+	int m = 2*img.rows;
+	int n = 2*img.cols;
+
+	//Função para copiar a imagem e aumentar a borda, inserindo 0's ao redor da imagem.
+	copyMakeBorder(img, expanded_img, 0, m - img.rows, 0, n - img.cols, BORDER_CONSTANT, Scalar::all(0));
+
+	//imshow("Processamento Digital de Imagens", teste);
+
+	return expanded_img;
+}
+
+
+void dft(Mat img, string source){
+	Mat dft_img;
+
+	dft_img = expandImgSize(img);
+
+	imshow("Processamento Digital de Imagens", dft_img);
+}
+
 
 int main(int argc, int **argv){
 
-	//Nome da Imagem
-	//const char *filename = "lena.bmp";
+	//Nome da imagem
+	string source = "../Debug/lenastripes.bmp";
 
 	//Carregar imagem dentro da matriz
 	Mat img;
-	img = cvLoadImage("C:/Users/Alan/Documents/Visual Studio 2010/Projects/opencv/Debug/lena.bmp");
+	img = imread(source);
 
 	//Verifica carregamento
 	if (img.empty()){
-		fprintf(stderr, "Erro: imagem %s nao encontrada.\n", "lena.bmp");
+		fprintf(stderr, "Erro: imagem nao encontrada.\n");
 		getchar();
 		exit(1);
 	}
@@ -21,9 +48,9 @@ int main(int argc, int **argv){
 	//Cria nova janela
 	namedWindow("Processamento Digital de Imagens", CV_WINDOW_AUTOSIZE);
 
-	//Mostrar imagem
-	imshow("Processamento Digital de Imagens", img);
-
+	//Transformada Discreta de Fourier
+	dft(img, source);
+	
 	//Espera click
 	waitKey();
 
