00d9caa33151d8abdb51a5732a9f77a49dde0160
brunoluizs <y.bruno@live.com>
2019-04-06 19:36:00 -0300

    1.2: imagem multiplicada por (-1)^x+y

--- opencv/main.cpp
+++ opencv/main.cpp
@@ -4,7 +4,30 @@
 using namespace cv;
 using namespace std;
 
+
+Mat centerTransform(Mat img){
+	unsigned char* data;
+	int i, j;
+	long long int pos = 0;
+
+	data = img.ptr(0);
+
+	//Aplicar o (-1)^(x+y) a todos os 3 canais
+	for (i = 0; i < img.rows*3; i++){
+		for (j = 0; j < img.cols; j++){
+			data[pos] = data[pos] * pow((double) -1, i+j);
+			pos += 1;
+		}
+	}
+
+	Mat centered_transform(img.rows, img.cols, CV_8UC3, data);
+
+	return centered_transform;
+}
+
+
 Mat expandImgSize(Mat img){
+	Mat centered_img = centerTransform(img);
 	Mat expanded_img;
 
 	//Funções para o cálculo do tamanho ideal para a DFT
@@ -20,23 +43,24 @@ Mat expandImgSize(Mat img){
 }
 
 
-void dft(Mat img, string source){
-	Mat dft_img;
+void filter(Mat img){
+	//Divisão dos planos REAL [0] e COMPLEXO [1]
+	Mat dft_img[2];
 
-	dft_img = expandImgSize(img);
+	dft_img[0] = expandImgSize(img);
+	dft_img[1] = dft_img[1].zeros(dft_img[0].rows, dft_img[0].cols, CV_8U);
 
-	imshow("Processamento Digital de Imagens", dft_img);
+	imshow("Processamento Digital de Imagens", dft_img[0]);
 }
 
 
 int main(int argc, int **argv){
 
 	//Nome da imagem
-	string source = "../Debug/lenastripes.bmp";
+	string source = "../Debug/lena.bmp";
 
 	//Carregar imagem dentro da matriz
-	Mat img;
-	img = imread(source);
+	Mat img = imread(source);
 
 	//Verifica carregamento
 	if (img.empty()){
@@ -48,8 +72,14 @@ int main(int argc, int **argv){
 	//Cria nova janela
 	namedWindow("Processamento Digital de Imagens", CV_WINDOW_AUTOSIZE);
 
+	//Show
+	/*imshow("Processamento Digital de Imagens", img_in);
+	waitKey();
+	imshow("Processamento Digital de Imagens", img);
+	waitKey();*/
+
 	//Transformada Discreta de Fourier
-	dft(img, source);
+	filter(img);
 	
 	//Espera click
 	waitKey();
